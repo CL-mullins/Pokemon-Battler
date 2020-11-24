45e1e201ae24508983521581fd494c09fa017196
Christopher Mullins <69275653+CL-mullins@users.noreply.github.com>
2020-11-24 14:46:39 -0800

    finalized basic type dictionary

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 class Pokemon:
     def __init__(self, name, HP, Damage, type):
-        typeDict = {'fire': {'fire': 0.5, 'grass': 2.0, 'water':0.5}} #determines the type effectiveness for the attacking Pokemon
+        typeDict = {'fire': 
+        {'fire': 0.5, 'grass': 2.0, 'water':0.5}
+        'grass':{'fire':0.5,'grass': 0.5, 'water':2.0}
+        'water': {'fire':2.0, 'grass' 0.5, 'water': 0.5}} #determines the type effectiveness for the attacking Pokemon
         self.name = name #sets the name of the Pokemon
         self.hp = HP #sets the hit points of the Pokemon
         self.Damage = Damage #sets the damage of the Pokemon (might not keep it)
         self.type = type #determines type effectiveness of attack
+
+
+    #I want an add attack method that also uses the type dictionary
+    #Also stats, I have two weeks

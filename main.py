0dc2386d9b6e711b571a3abbfbc6a78e6782141a
Christopher Mullins <69275653+CL-mullins@users.noreply.github.com>
2020-11-27 15:17:18 -0800

    updated comments

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#Pokemon object that can battle
 class Pokemon(object):
     typeChart = {'fire': 
         {'fire': 0.5, 'grass': 2.0, 'water':0.5},
@@ -6,10 +7,24 @@ class Pokemon(object):
          #determines the type effectiveness for the attacking Pokemon
     def __init__(self, name, HP, Damage, type): 
         self.name = name #sets the name of the Pokemon
-        self.hp = HP #sets the hit points of the Pokemon
+        self.hp = HP #sets the max hit points of the Pokemon
+        self.current_HP = HP #sets current HP of Pokemon
         self.Damage = Damage #sets the damage of the Pokemon (might not keep it)
         self.type = type #determines type effectiveness of attack
+        self.moves = list()
 
+    def isAlive(self):
+        #Checks to see is the Pokemon has or has not fainted
+        if self.current_HP <= 0:
+            return False
+        else:
+            return True
+
+    #def take_damage(sef):
+
+
+
+squirtle = Pokemon('Squirtle', 100, 5, 'water')
 
     #I want an add attack method that also uses the type dictionary
-    #Also stats, I have two weeks
+    #Also stats, I have two weeks. I want there to be a window with Sprites as well

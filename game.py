638548455fb2017e908d0ddfae21a22479f8d30d
Christopher Mullins <69275653+CL-mullins@users.noreply.github.com>
2021-01-11 15:15:52 -0800

    cursor works!

--- game.py
+++ game.py
@@ -91,6 +91,7 @@ class Selector(pygame.sprite.Sprite):
         self.movey = 0 #Move along Y
         self.frame = 0 #Count frames
 
+
     #Draws image to screen
     def draw(self,surface,destination):
         surface.blit(self.image,destination)
@@ -159,7 +160,23 @@ class TaskBar(pygame.sprite.Sprite):
         pygame.transform.scale(self.image,(wdth,hght))
 
 
-while running:
+    '''Controller Functionality'''
+arrayX = [{'cursor':(578,470),'action': 'Attack'},{'cursor':(820,470),'action':'Bag'}] #Attack or Bag # Selects the specific function
+arrayY = [{'cursor':(578,500),'action':'Pokemon',},{'cursor':(820,500),'action':'Run'}] #Pokemon or RUn
+controller = [arrayX,arrayY]
+
+indexX = 0
+indexY = 0 #Chooses if we select from array x or array y
+
+def helper(num): #Holds everything within either 0 or 1
+    print('Helper was called')
+    if num < 0:
+        return 1
+    elif num >= 2:
+        return 0
+    else:
+        return num
+
     clock.tick(FPS)
 
         
@@ -167,132 +184,143 @@ while running:
     # Add & Configure Data from Pokemon CSV
     # as to Pokemon name, stats and type(s)
 
-    df = pd.read_csv('dataframes/pokemon.csv')
-    pokeName = df['Name']
-    pokeType1 = df['Type 1']
-    pokeAttack = df['Attack']
-    pokeDefense = df['Defense']
-    pokeSpAttack = df['Sp. Atk']
-    pokeSpDefense = df['Sp. Def']
-    pokeSpeed = df['Speed']
+df = pd.read_csv('dataframes/pokemon.csv')
+pokeName = df['Name']
+pokeType1 = df['Type 1']
+pokeAttack = df['Attack']
+pokeDefense = df['Defense']
+pokeSpAttack = df['Sp. Atk']
+pokeSpDefense = df['Sp. Def']
+pokeSpeed = df['Speed']
 
-    # For some reason you take row index number and subtract 2
+# For some reason you take row index number and subtract 2
 
-    # Initialize battling Pokemon
+# Initialize battling Pokemon
 
-    def locatePokemon():
-        '''Finds Pokemon within CSV'''
-        # Enter name of Pokemon that you wish to find
-        pokeFinder = input("Enter a Pokemon's name!: ")
-        # Enters that Pokemon's name into function that finds the Pokemon's row number
-        # relative to the csv file
-        # Theres probably another way to do this..
-        pokeLocation = df.index[df["Name"].str.contains(pokeFinder) == True].tolist()
-        # Pull Pokemon's row number from index # ___
-        return pokeLocation[0]
+def locatePokemon():
+    '''Finds Pokemon within CSV'''
+    # Enter name of Pokemon that you wish to find
+    pokeFinder = input("Enter a Pokemon's name!: ")
+    # Enters that Pokemon's name into function that finds the Pokemon's row number
+    # relative to the csv file
+    # Theres probably another way to do this..
+    pokeLocation = df.index[df["Name"].str.contains(pokeFinder) == True].tolist()
+    # Pull Pokemon's row number from index # ___
+    return pokeLocation[0]
 
-    # Mechanics decision, make it Pokemon Showdown-esque, that way I can work backwards from already having a comprehensive
-    # combat system and add overworld if I want to!
+# Mechanics decision, make it Pokemon Showdown-esque, that way I can work backwards from already having a comprehensive
+# combat system and add overworld if I want to!
 
-    squirtle = Pokemon(pokeName[9], 100, pokeType1[9], pokeAttack[9], pokeDefense[9], pokeSpAttack[9], pokeSpDefense[9], pokeSpeed[9], 1, "/Users/chrismullins/dev/courses/cs1.1/Pokemon-Battler/sprites/squirtle.png")
-    charmander = Pokemon(pokeName[4], 100, pokeType1[4], pokeAttack[4], pokeDefense[4], pokeSpAttack[4], pokeSpDefense[4], pokeSpeed[4],1, "/Users/chrismullins/dev/courses/cs1.1/Pokemon-Battler/sprites/charmander.png")
+squirtle = Pokemon(pokeName[9], 100, pokeType1[9], pokeAttack[9], pokeDefense[9], pokeSpAttack[9], pokeSpDefense[9], pokeSpeed[9], 1, "/Users/chrismullins/dev/courses/cs1.1/Pokemon-Battler/sprites/squirtle.png")
+charmander = Pokemon(pokeName[4], 100, pokeType1[4], pokeAttack[4], pokeDefense[4], pokeSpAttack[4], pokeSpDefense[4], pokeSpeed[4],1, "/Users/chrismullins/dev/courses/cs1.1/Pokemon-Battler/sprites/charmander.png")
 
 
-    # Add moves
-    # TODO: Considering automating the process of adding moves by finding an appropriate moves csv
+# Add moves
+# TODO: Considering automating the process of adding moves by finding an appropriate moves csv
 
-    # initialize squirtle moveset
-    WaterGun = Moves("Water Gun", 'Water', 40, 'Special', 25)
-    bubble = Moves("Bubble", 'Water', 40, 'Special', 25)
-    scratch = Moves("Scratch", 'Normal', 15, 'Physical', 25)
-    growl = Moves("Growl", 'Normal', 15, 'Physical', 25)
+# initialize squirtle moveset
+WaterGun = Moves("Water Gun", 'Water', 40, 'Special', 25)
+bubble = Moves("Bubble", 'Water', 40, 'Special', 25)
+scratch = Moves("Scratch", 'Normal', 15, 'Physical', 25)
+growl = Moves("Growl", 'Normal', 15, 'Physical', 25)
 
-    # initialize charmander moveset
-    FireSpin = Moves("Fire Spin", 'Fire', 25, 'Special', 25)
-    flamethrower = Moves('Flamethrower', 'Fire', 50, 'Special', 25)
+# initialize charmander moveset
+FireSpin = Moves("Fire Spin", 'Fire', 25, 'Special', 25)
+flamethrower = Moves('Flamethrower', 'Fire', 50, 'Special', 25)
 
-    # initialize item
-    # TODO: Considering having an effect be a function or method called within the parameters of 
-    # TODO: the item call
-    potion = Items('Potion', 20)
-    superpotion = Items('Super Potion', 50)
+# initialize item
+# TODO: Considering having an effect be a function or method called within the parameters of 
+# TODO: the item call
+potion = Items('Potion', 20)
+superpotion = Items('Super Potion', 50)
 
-    # Squirtle Add Moves
-    squirtle.add_move(WaterGun)
-    squirtle.add_move(bubble)
-    squirtle.add_move(scratch)
-    squirtle.add_move(growl)
+# Squirtle Add Moves
+squirtle.add_move(WaterGun)
+squirtle.add_move(bubble)
+squirtle.add_move(scratch)
+squirtle.add_move(growl)
 
-    # Squirtle Add Item
-    squirtle.addItem(potion)
+# Squirtle Add Item
+squirtle.addItem(potion)
 
-    # Charmander Add Moves
-    charmander.add_move(scratch)
-    charmander.add_move(growl)
-    charmander.add_move(FireSpin)
-    charmander.add_move(flamethrower)
+# Charmander Add Moves
+charmander.add_move(scratch)
+charmander.add_move(growl)
+charmander.add_move(FireSpin)
+charmander.add_move(flamethrower)
 
-    '''Initializes  Pokemon as Players'''
-    #Player
-    player = Player(squirtle.image) #96 x 96 Image Resolution
-    #Opponent
-    player2 = Player(charmander.image) #96 x 96 Image Resolution
-    #Resize (scale) sprite for game window resolution
-    '''Scales Pokemon to Screen '''
+'''Initializes  Pokemon as Players'''
+#Player
+player = Player(squirtle.image) #96 x 96 Image Resolution
+#Opponent
+player2 = Player(charmander.image) #96 x 96 Image Resolution
+#Resize (scale) sprite for game window resolution
+'''Scales Pokemon to Screen '''
 
-    player.image = pygame.transform.scale(player.image,(384,384))
-    player2.image = pygame.transform.scale(player2.image, (384,384))
+player.image = pygame.transform.scale(player.image,(384,384))
+player2.image = pygame.transform.scale(player2.image, (384,384))
 
-    '''Draws Pokemon onto Screen '''
-    #Draws player pokemon
-    player.draw(screen,(5,250))
-    #Draws opponent pokemon
-    player2.draw(screen,(575,60))
-    #Destination of player's Pokemon should be determined by the battle location spot
+'''Draws Pokemon onto Screen '''
+#Draws player pokemon
+player.draw(screen,(5,250))
+#Draws opponent pokemon
+player2.draw(screen,(575,60))
+#Destination of player's Pokemon should be determined by the battle location spot
 
-    '''Draws HP Bars onto Screen '''
-    #Player HP Bar
-    playerHPBar = HPBar("/Users/chrismullins/dev/courses/cs1.1/Pokemon-Battler/sprites/[T]PlayerHPBar.png")
-    playerHPBar.image = pygame.transform.scale(playerHPBar.image,(300,75))
-    playerHPBar.draw(screen,(700,364))
+'''Draws HP Bars onto Screen '''
+#Player HP Bar
+playerHPBar = HPBar("/Users/chrismullins/dev/courses/cs1.1/Pokemon-Battler/sprites/[T]PlayerHPBar.png")
+playerHPBar.image = pygame.transform.scale(playerHPBar.image,(300,75))
+playerHPBar.draw(screen,(700,364))
 
-    #Opponent HP Bar
-    opponentHPBar = HPBar("/Users/chrismullins/dev/courses/cs1.1/Pokemon-Battler/sprites/[T]OpponentHPBar.png")
-    opponentHPBar.image = pygame.transform.scale(opponentHPBar.image,(300,75))
-    opponentHPBar.draw(screen,(200,125))
+#Opponent HP Bar
+opponentHPBar = HPBar("/Users/chrismullins/dev/courses/cs1.1/Pokemon-Battler/sprites/[T]OpponentHPBar.png")
+opponentHPBar.image = pygame.transform.scale(opponentHPBar.image,(300,75))
+opponentHPBar.draw(screen,(200,125))
 
-    '''Draw text screen'''
-    #Player text screen
-    MainBar = TextBar("/Users/chrismullins/dev/courses/cs1.1/Pokemon-Battler/sprites/BaseTextMenu.png")
+'''Draw text screen'''
+#Player text screen
+MainBar = TextBar("/Users/chrismullins/dev/courses/cs1.1/Pokemon-Battler/sprites/BaseTextMenu.png")
 
-    #Scale MainBar
-    MainBar.image = pygame.transform.scale(MainBar.image,(width,125))
-    MainBar.draw(screen, (0,439))
+#Scale MainBar
+MainBar.image = pygame.transform.scale(MainBar.image,(width,125))
+MainBar.draw(screen, (0,439))
 
-    taskBar = TaskBar("/Users/chrismullins/dev/courses/cs1.1/Pokemon-Battler/sprites/TaskMenu.png") #119 x 47
-    taskBar.image = pygame.transform.scale(taskBar.image,(450,125))
-    taskBar.draw(screen, (550,439))
+taskBar = TaskBar("/Users/chrismullins/dev/courses/cs1.1/Pokemon-Battler/sprites/TaskMenu.png") #119 x 47
+taskBar.image = pygame.transform.scale(taskBar.image,(450,125))
+taskBar.draw(screen, (550,439))
 
-    '''Initialize Menu Text'''
-    oppName_display(f"{charmander.name}")
-    oppLevel_display(f"{charmander.level}")
-    playerName_display(f"{squirtle.name}")
-    playerLevel_display(f"{squirtle.level}")
-    playerHP_display(f"{squirtle.current_HP} / {squirtle.hp}")
-    message_display(f"What will {squirtle.name} do?")
+'''Initialize Menu Text'''
+oppName_display(f"{charmander.name}")
+oppLevel_display(f"{charmander.level}")
+playerName_display(f"{squirtle.name}")
+playerLevel_display(f"{squirtle.level}")
+playerHP_display(f"{squirtle.current_HP} / {squirtle.hp}")
+message_display(f"What will {squirtle.name} do?")
 
-    '''Initialize Selector'''
-    selector = Selector("/Users/chrismullins/dev/courses/cs1.1/Pokemon-Battler/sprites/selectionPicker.png")
-    selector.image = pygame.transform.scale(selector.image,(24,30))
-    selector.draw(screen, (578, 470))
+'''Initialize Selector'''
+selector = Selector("/Users/chrismullins/dev/courses/cs1.1/Pokemon-Battler/sprites/selectionPicker.png")
+selector.image = pygame.transform.scale(selector.image,(24,30))
+#selector.draw(screen, (578, 470))
+
+while running:
+
     
-
-    pygame.display.update()
-    #squirtle.battle(charmander)
+    #pygame.display.update()
         # I want an add attack method that also uses the type dictionary
         # Also stats, I have two weeks. I want there to be a window with Sprites as well
+    
+
+
+    #if keypress == 'w': #Calls the function
+        #indexY = helper(indexY - 1) #Limits between top or bottom row
+        #selector.draw(screen, controller[indexY][indexX]['cursor']
+
+    #if keypress == 's':
+    #   indexY = helper(indexY + 1)
 
         
+
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -303,18 +331,36 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == ord('w'):
                 #Move selector up, prepare for selection
-                selector.draw(screen, (578, 500))
-                selector.control(0,30)
+                #selector.kill()
+                indexY = helper(indexY - 1) #Limits between top or bottom row
+                taskBar.draw(screen, (550,439))
+                selector.draw(screen, controller[indexY][indexX]['cursor'])
                 
-                pass
+                #Needs to stay a specific place
+                #selector.control(0,30)
             if event.key == ord('a'):
                 #Move selector left
-                pass
+                indexX = helper(indexX - 1) #Limits between top or bottom row
+                taskBar.draw(screen, (550,439))
+                selector.draw(screen, controller[indexY][indexX]['cursor'])               
+
             if event.key == ord('s'):
-                #Move selector down:
-                pass
+                indexY = helper(indexY + 1) #Limits between top or bottom row
+                taskBar.draw(screen, (550,439))
+                selector.draw(screen, controller[indexY][indexX]['cursor'])
             if event.key == ord('d'):
-                #Move selector right
-                pass
-        selector.update()
-        pygame.display.update()
+                indexX = helper(indexX + 1) #Limits between top or bottom row
+                taskBar.draw(screen, (550,439))
+                selector.draw(screen, controller[indexY][indexX]['cursor'])
+            if event.key == ord('\r'):
+                action = controller[indexY][indexX]['action']
+                if action == 'Attack':
+                    print('We made it! Back end time!')
+                if action == 'Bag':
+                    print('We made it! Back end time!')
+                if action == 'Run':
+                    print('We made it! Back end time!')
+                if action == 'Pokemon':
+                    print('We made it! Back end time!')
+    pygame.display.update()
+        
